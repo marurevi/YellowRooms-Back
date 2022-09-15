ef646f4bc7eb51efeb4c8c9a0dd21de210aefcdc
Andres Agudelo <anagudelogu@gmail.com>
2022-09-15 14:16:31 -0500

    Create relationships between models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  has_many :reservations
+  has_many :rooms, through: :reservations
 end
