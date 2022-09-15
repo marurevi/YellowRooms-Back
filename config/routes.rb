811afc2b267b122a0280b38a3bd9f525b2047bfd
Andres Agudelo <anagudelogu@gmail.com>
2022-09-15 14:24:45 -0500

    Define initial namespaced routes and scopes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  namespace :api do
+    namespace :v1 do
+      
+    end
+  end
+  
+  scope :api do
+    scope :v1 do
+      devise_for :users,
+                 path: '',
+                 path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' },
+                 defaults: { format: :json }
+    end
+  end
 end
