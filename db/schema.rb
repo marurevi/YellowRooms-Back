fafe70b91e110968c8bbdaa81697f9e1bb32328a
Andres Agudelo <anagudelogu@gmail.com>
2022-09-15 14:05:39 -0500

    Create Room model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_15_185008) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_15_190306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "stars", null: false
+    t.integer "persons_allowed", null: false
+    t.string "photo", null: false
+    t.text "description", null: false
+    t.float "price", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
