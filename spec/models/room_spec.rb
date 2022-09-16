af46b08078f1a347ec61f684e642c9eb16cff816
marurevi <maryrev@hotmail.com>
2022-09-16 19:07:30 -0300

    Add room's model validations and modify tests

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,39 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
+  let(:room) { FactoryBot.create(:room) }
   describe 'Validations' do
     it 'is valid with valid attributes' do
-      room = FactoryBot.build(:room)
       expect(room).to be_valid
     end
 
     it 'is not valid without a name' do
-      room = FactoryBot.build(:room, name: nil)
+      room.name = nil
       expect(room).to_not be_valid
     end
 
     it 'is not valid without a stars' do
-      room = FactoryBot.build(:room, stars: nil)
+      room.stars = nil
+      expect(room).to_not be_valid
+    end
+
+    it 'is not valid with a non integer stars' do
+      room.stars = 3.5
       expect(room).to_not be_valid
     end
 
     it 'is not valid without a persons_allowed' do
-      room = FactoryBot.build(:room, persons_allowed: nil)
+      room.persons_allowed = nil
       expect(room).to_not be_valid
     end
 
     it 'is not valid without a photo' do
-      room = FactoryBot.build(:room, photo: nil)
+      room.photo = nil
+      expect(room).to_not be_valid
+    end
+
+    it 'is not valid with a non url photo' do
+      room.photo = 'photo'
       expect(room).to_not be_valid
     end
 
     it 'is not valid without a description' do
-      room = FactoryBot.build(:room, description: nil)
+      room.description = nil
       expect(room).to_not be_valid
     end
 
     it 'is not valid without a price' do
-      room = FactoryBot.build(:room, price: nil)
+      room.price = nil
+      expect(room).to_not be_valid
+    end
+
+    it 'is not valid with a non numeric price' do
+      room.price = 'price'
       expect(room).to_not be_valid
     end
   end
