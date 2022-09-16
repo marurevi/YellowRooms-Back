f84ce83ae14086ccee3b6c91e80462105f83f899
Andres Agudelo <anagudelogu@gmail.com>
2022-09-16 16:00:31 -0500

    Fix wrong 'sign_up' path_name, changed it for 'registration'

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     scope :v1 do
       devise_for :users,
                  path: '',
-                 path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' },
+                 path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register' },
                  defaults: { format: :json }
     end
   end
