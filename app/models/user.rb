b04ea5495e958a44968840aef0c405590277f67c
Andres Agudelo <anagudelogu@gmail.com>
2022-09-16 16:19:43 -0500

    Add validations for the user's username with their specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
 
   has_many :reservations
   has_many :rooms, through: :reservations
+
+  validates :username, presence: true, uniqueness: true,
+                       format: { with: /\A[a-zA-Z]+\z/, message: 'only allows letters' }
 end
