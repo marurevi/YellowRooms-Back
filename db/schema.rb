8b227577c27584a48146d2d3069ecac81c1e6a6a
marurevi <maryrev@hotmail.com>
2022-09-16 12:55:15 -0300

    Add column deleted on Rooms table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_15_191033) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_16_155054) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_15_191033) do
     t.float "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "deleted", default: false
   end
 
   create_table "users", force: :cascade do |t|
