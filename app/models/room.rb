c3ff613a5609e134a26da1ec4cab405457995996
marurevi <maryrev@hotmail.com>
2022-09-16 12:57:29 -0300

    Create rooms model test and validations

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,11 @@
 class Room < ApplicationRecord
   has_many :reservations
   has_many :users, through: :reservations
+
+  validates :name, presence: true
+  validates :stars, presence: true
+  validates :persons_allowed, presence: true
+  validates :photo, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
 end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      room = FactoryBot.build(:room)
+      expect(room).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      room = FactoryBot.build(:room, name: nil)
+      expect(room).to_not be_valid
+    end
+
+    it 'is not valid without a stars' do
+      room = FactoryBot.build(:room, stars: nil)
+      expect(room).to_not be_valid
+    end
+
+    it 'is not valid without a persons_allowed' do
+      room = FactoryBot.build(:room, persons_allowed: nil)
+      expect(room).to_not be_valid
+    end
+
+    it 'is not valid without a photo' do
+      room = FactoryBot.build(:room, photo: nil)
+      expect(room).to_not be_valid
+    end
+
+    it 'is not valid without a description' do
+      room = FactoryBot.build(:room, description: nil)
+      expect(room).to_not be_valid
+    end
+
+    it 'is not valid without a price' do
+      room = FactoryBot.build(:room, price: nil)
+      expect(room).to_not be_valid
+    end
+  end
 end
