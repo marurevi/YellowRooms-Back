47f8c28f163fd88e7fce95c5e44142d3f624af29
Andres Agudelo <anagudelogu@gmail.com>
2022-09-19 11:07:08 -0500

    Implement logout feature:
    - Updated devise-jwt gem because a new update was causing some errors

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -12,4 +12,22 @@ class Api::V1::Users::SessionsController < Devise::SessionsController
       }
     }, status: :ok
   end
+
+  def respond_to_on_destroy
+    if current_user
+      render json: {
+        code: 200,
+        data: {
+          message: 'logged out successfully'
+        }
+      }, status: :ok
+    else
+      render json: {
+        code: 401,
+        data: {
+          message: "Couldn't find an active session."
+        }
+      }, status: :unauthorized
+    end
+  end
 end
