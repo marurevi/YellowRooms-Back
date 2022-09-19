73441171a1c58f9ae3caa841be8c4a605373178b
marurevi <maryrev@hotmail.com>
2022-09-19 17:42:36 -0300

    Implement validations acording to model rspec

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,20 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :room
+
+  validates :user, presence: true
+  validates :room, presence: true
+  validates :start_date, :end_date, presence: true
+  validates_comparison_of :start_date, greater_than_or_equal_to: Date.today
+  validates_comparison_of :end_date, greater_than: :start_date
+  validates :city, presence: true
+
+  validate :available?
+
+  def available?
+    return if room.blank?
+
+    errors.add(:room, 'is not available') if room.reservations.where('start_date <= ? AND end_date >= ?', end_date,
+                                                                     start_date).any?
+  end
 end
