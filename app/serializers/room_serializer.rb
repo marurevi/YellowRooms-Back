12979945e8cb147c71f78b2b8d79cbb2ad01e602
marurevi <maryrev@hotmail.com>
2022-09-20 18:50:42 -0300

    Modify room serializer

--- app/serializers/room_serializer.rb
+++ app/serializers/room_serializer.rb
@@ -1,6 +1,7 @@
 class RoomSerializer
   include JSONAPI::Serializer
   has_many :reservations
+  has_many :users, through: :reservations
 
   attributes :id, :name, :stars, :persons_allowed, :photo, :description, :price
 end
