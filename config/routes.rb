2a6dc2ba89b7e463cd20ff5b2891f3b65e1098cb
marurevi <maryrev@hotmail.com>
2022-09-19 22:16:22 -0300

    Create reservations endpoints routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: { format: :json } do
       resources :rooms, only: [:index, :create, :destroy]
+      resources :reservations, only: [:index, :create, :destroy]
     end
   end
   
