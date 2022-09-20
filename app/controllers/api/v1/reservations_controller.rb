8ecfe6c93709966ed3107593585147c71481e9b3
marurevi <maryrev@hotmail.com>
2022-09-19 22:19:57 -0300

    Create a controller to implement the CRUD actions

---
+++ app/controllers/api/v1/reservations_controller.rb
@@ -0,0 +1,38 @@
+class Api::V1::ReservationsController < ApplicationController
+  def index
+    @reservations = Reservation.all
+    if @reservations.empty?
+      render json: { error: "Couldn't find Reservation" }, status: :not_found
+    else
+      render json: { code: 200, data: @reservations }, status: :ok
+    end
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save
+      render json: { code: 201, data: @reservation }, status: :created
+    else
+      render json: { code: 422, data: { errors: @reservation.errors.full_messages.to_sentence } },
+             status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @reservation = Reservation.find_by(id: params[:id])
+    if @reservation.nil?
+      render json: { error: "Couldn't find Reservation with 'id'=#{params[:id]}" }, status: :not_found
+    elsif @reservation.destroy
+      render json: { code: 200, data: { message: 'Reservation deleted' } }, status: :ok
+    else
+      render json: { code: 422, data: { errors: @reservation.errors.full_messages.to_sentence } },
+             status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date, :user_id, :room_id, :city)
+  end
+end
