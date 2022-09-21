e40658f2c6057c668bace72a568b66aa6358c08c
Andres Agudelo <anagudelogu@gmail.com>
2022-09-20 19:07:40 -0500

    Update reservations specs to include authentication

--- spec/requests/reservations_spec.rb
+++ spec/requests/reservations_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Reservations', type: :request do
+  let(:user) { create(:user) }
+  let(:headers) { generate_jwt_token_for(user) }
+
   describe 'GET /api/v1/reservations' do
-    before { get api_v1_reservations_path }
+    before { get api_v1_reservations_path, headers: }
 
     context 'when there are no reservations' do
       it 'returns a 404 status code' do
@@ -29,7 +32,7 @@ RSpec.describe 'Reservations', type: :request do
                                    end_date: Date.today + 3.day)
         @r3 = create(:reservation, user: @user3, room: @room3, start_date: Date.today,
                                    end_date: Date.today + 2.day)
-        get api_v1_reservations_path
+        get api_v1_reservations_path, headers:
       end
 
       it 'returns http success' do
@@ -54,19 +57,26 @@ RSpec.describe 'Reservations', type: :request do
         { reservation: { start_date: '', end_date: '', user_id: 1, room_id: 1 } }
       end
       it 'does not create a new reservation' do
-        expect { post api_v1_reservations_path, params: invalid_params }.to change(Reservation, :count).by(0)
+        expect do
+          post api_v1_reservations_path, params: JSON.generate(invalid_params), headers:
+        end.to change(Reservation, :count).by(0)
       end
       it 'returns http unprocessable entity' do
-        post api_v1_reservations_path, params: invalid_params
+        post api_v1_reservations_path, headers: headers, params: JSON.generate(invalid_params)
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
     context 'when the params are valid' do
       before do
-        @user = create(:user)
         @room = create(:room)
-        post api_v1_reservations_path,
-             params: { reservation: FactoryBot.attributes_for(:reservation, user_id: @user.id, room_id: @room.id) }
+        post api_v1_reservations_path, headers:,
+                                       params: JSON.generate({
+                                                               reservation: attributes_for(
+                                                                 :reservation,
+                                                                 user_id: user.id,
+                                                                 room_id: @room.id
+                                                               )
+                                                             })
       end
       it 'returns http created' do
         expect(response).to have_http_status(:created)
@@ -77,7 +87,7 @@ RSpec.describe 'Reservations', type: :request do
 
       it 'returns the correct reservation' do
         data = JSON.parse(response.body)['data']
-        expect(data['user_id']).to eq(@user.id)
+        expect(data['user_id']).to eq(user.id)
         expect(data['room_id']).to eq(@room.id)
       end
     end
@@ -86,16 +96,15 @@ RSpec.describe 'Reservations', type: :request do
   describe 'DELETE /api/v1/reservations/:id' do
     context 'when the reservation does not exist' do
       it 'returns http not found' do
-        delete api_v1_reservation_path(1)
+        delete api_v1_reservation_path(1), headers: headers
         expect(response).to have_http_status(:not_found)
       end
     end
     context 'when the reservation exists' do
       before do
-        @user = create(:user)
         @room = create(:room)
-        @reservation = create(:reservation, user: @user, room: @room)
-        delete api_v1_reservation_path(@reservation)
+        @reservation = create(:reservation, user:, room: @room)
+        delete api_v1_reservation_path(@reservation), headers:
       end
       it 'returns http success' do
         expect(response).to have_http_status(:success)
